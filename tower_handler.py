ffca0b57fd56658ffd64ce1116968f1877ac0b14
Mauricio Teixeira <mauricio.teixeira@gmail.com>
2018-09-21 17:59:56 -0400

    fix author and add web site reference

--- tower_handler.py
+++ tower_handler.py
@@ -11,7 +11,7 @@
 #
 # The above copyright notice and this permission notice shall be included
 # in all copies or substantial portions of the Software.
-
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -24,7 +24,8 @@
 # Nagios Service Handler. It will trigger an
 # Ansible Tower job template.
 #
-# Author: Mauricio Teixeira <mteixeira@redhat.com>
+# Web site: https://github.com/ansible/tower-nagios-integration
+# Author: https://github.com/badnetmask
 
 import sys
 import json
